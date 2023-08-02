b68401b6309c1129c1d0d2218ed76fb447ee3f4f
Aryan Salemabadi <54766308+aryan-salemababdi@users.noreply.github.com>
2023-08-02 23:20:21 +0330

    final commit

--- stores/store.ts
+++ stores/store.ts
@@ -1,13 +1,18 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 
 import templateSlice  from "./slices/templateSlice/templateSlice";
 
+
+export type RootState = ReturnType<typeof store.getState>;
+
+import logger from "redux-logger";
 
 const store = configureStore({
 
     reducer:{
         counter:templateSlice
-    }
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger)
 
 
 });
