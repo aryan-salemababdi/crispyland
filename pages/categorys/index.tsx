ffc31b56b457e8069fcd89ad890c9ec85444a4ad
Aryan Salemabadi <54766308+aryan-salemababdi@users.noreply.github.com>
2023-08-08 13:37:22 +0330

    final commit

---
+++ pages/categorys/index.tsx
@@ -0,0 +1,56 @@
+import { NextPage } from "next";
+import CategorysPage from "../../components/temp/CategorysPage/CategorysPage";
+
+interface MenuItem {
+
+    id:string;
+    img:string;
+    name:string;
+    dsc:string;
+    price:number;
+    rate:number;
+    country:string;
+    
+}
+
+interface Menu {
+
+    data : MenuItem[];
+
+}
+
+const Categorys:NextPage<Menu> = ({data}) => {
+
+    
+  return (
+    <div 
+    style={{height:"100%",width:"100%"}}
+    >
+        <CategorysPage
+        data = {data}
+        />
+     </div>
+  )
+}
+
+export default Categorys;
+
+export async function getServerSideProps(context:any){
+
+    const {
+        query: { rate },
+      } = context;
+
+    const res = await fetch("http://localhost:4000/fried-chicken");
+
+    const data = await res.json();
+
+      const filteredData = data.filter((item:any) => item.rate === parseInt(rate));
+
+    return{
+        props:{
+            data:filteredData
+        },
+    }
+
+};
